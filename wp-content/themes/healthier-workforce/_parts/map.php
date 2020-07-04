2927b11fc0273cca5724f9336939ca524d56b309
Alex James <alexjames@Alexs-MacBook-Pro-2.local>
2020-07-04 14:51:53 +0100

    Changes to map CTA for background image position and opacity

--- wp-content/themes/healthier-workforce/_parts/map.php
+++ wp-content/themes/healthier-workforce/_parts/map.php
@@ -1,3 +1,12 @@
 		<p class="title">UK-Wide Coverage</p>
 
+		<ul>
+			<li><i class="fa fa-check"></i> Birmingham</li>
+			<li><i class="fa fa-check"></i> Bristol</li>
+			<li><i class="fa fa-check"></i> London</li>
+			<li><i class="fa fa-check"></i> Manchester</li>
+			<li><i class="fa fa-check"></i> Liverpool</li>
+			<li><i class="fa fa-check"></i> Nottingham</li>
+		</ul>
+
 		<p class="phone"><i class="fa fa-mobile" aria-hidden="true"></i> <strong><span class="ld-phonenumber"><?php the_field('default_phone_number', 'option'); ?></span></strong></p>
