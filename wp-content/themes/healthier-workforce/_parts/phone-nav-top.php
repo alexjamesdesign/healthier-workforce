e183988d1f989cda4a24097333a8ddb83d98503e
Alex Dudley <alex.dudley@adtrak.co.uk>
2020-10-26 13:02:50 +0000

    Changed phone-top-right to be clickable

--- wp-content/themes/healthier-workforce/_parts/phone-nav-top.php
+++ wp-content/themes/healthier-workforce/_parts/phone-nav-top.php
@@ -1,6 +1,6 @@
 <div class="phone-nav-top Fixed">
 
-		<a class="stickyheader-left stickyheader-btn"><i class="fa fa-mobile" aria-hidden="true"></i> <?php do_action('ald_default'); ?></a>
+		<span class="stickyheader-left stickyheader-btn"><i class="fa fa-mobile" aria-hidden="true"></i> <?php do_action('ald_default'); ?></span>
 
     <a class="stickyheader-right stickyheader-btn menu-btn" href="#mmenu"><i class="fa fa-bars"></i> Menu</a>
 
