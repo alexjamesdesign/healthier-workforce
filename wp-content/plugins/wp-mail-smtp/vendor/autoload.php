c55f4aad389ed11a0bf6703a0128239541e00b1c
Alex James <hello@alexjamesdesign.co.uk>
2020-11-02 13:43:32 +0000

    Added ACF field to mobile to show location number

--- wp-content/plugins/wp-mail-smtp/vendor/autoload.php
+++ wp-content/plugins/wp-mail-smtp/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit72f613a3d0c2cc77892490951b6e5352::getLoader();
+return ComposerAutoloaderInit982434b9fa67b618e1fee7385b7ec239::getLoader();
--- wp-content/themes/healthier-workforce/_parts/phone-nav-top.php
+++ wp-content/themes/healthier-workforce/_parts/phone-nav-top.php
@@ -1,6 +1,18 @@
 <div class="phone-nav-top Fixed">
 
-		<span class="stickyheader-left stickyheader-btn"><i class="fa fa-mobile" aria-hidden="true"></i> <?php do_action('ald_default'); ?></span>
+    <?php 
+			$ld_location = get_field('location_name');
+			if($ld_location) : ?>
+
+        <span class="stickyheader-left stickyheader-btn"><i class="fa fa-mobile location-page-number" aria-hidden="true"></i> <?php do_action('ald_single', $ld_location); ?></span>
+
+			<?php else: ?>
+
+        <span class="stickyheader-left stickyheader-btn"><i class="fa fa-mobile" aria-hidden="true"></i> <?php do_action('ald_default'); ?></span>
+			
+      <?php endif;
+
+    ?>
 
     <a class="stickyheader-right stickyheader-btn menu-btn" href="#mmenu"><i class="fa fa-bars"></i> Menu</a>
 
