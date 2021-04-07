ad702a1e188b6aa0cb8ddc59a20691b2f243b08c
Simon Wade <simon.wade@adtrak.co.uk>
2021-04-07 20:19:19 +0100

    remove link to adtrak ld.js and add to site locally

--- wp-content/themes/healthier-workforce/_parts/ld.php
+++ wp-content/themes/healthier-workforce/_parts/ld.php
@@ -5,10 +5,10 @@ ld_var['ld_message'] = 'Call Locally on Mobile';
 
 </script>
 
-<script src="https://adtrakld.co.uk/ld.js"></script>
+<!-- <script src="https://adtrakld.co.uk/ld.js"></script> -->
 <?php /*<script>if(!window.ld_ready){document.write('<script src="ld/ld.js"><\/script>');document.write('<script src="http://adtrakld.co.uk/alert.php?url='+encodeURIComponent(location.href)+'&version=Unknown&message=Primary%20script%20fail"><\/script>');}</script>*/ ?>
 
-
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/_js/scripts/ld.js"></script>
 
 <!-- For location pages // NEEDS CUSTOM FIELD TO WORK -->
 <?php if (is_singular('locations')) : ?>
