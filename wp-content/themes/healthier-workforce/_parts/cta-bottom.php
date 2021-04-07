4eca09db8c068866aaf208542a040e3404370c91
Simon Wade <simon.wade@adtrak.co.uk>
2021-04-07 20:13:31 +0100

    add location to number ppc only

--- wp-content/themes/healthier-workforce/_parts/cta-bottom.php
+++ wp-content/themes/healthier-workforce/_parts/cta-bottom.php
@@ -2,6 +2,11 @@
 
 	<p class="title">Get in touch and find out how we can help your people and business</p>
 
-	<p class="phone"><i class="fa fa-mobile" aria-hidden="true"></i> <strong><?php do_action('ald_default'); ?></strong></p>
+
+	<?php if ( (isset($_COOKIE['area']) && $_COOKIE['area'] !='uk') || (isset($_GET['a']) && $_GET['a'] !='uk')) : ?>
+        <p class="phone"><i class="fa fa-mobile" aria-hidden="true"></i> <strong><?php echo do_action('ctm_location'); ?> <?php do_action('ald_default'); ?></strong></p>
+    <?php else: ?>
+        <p class="phone"><i class="fa fa-mobile" aria-hidden="true"></i> <strong><?php do_action('ald_default'); ?></strong></p>
+    <?php endif; ?>
 
 </div>
--- wp-content/themes/healthier-workforce/_parts/free-quotation-advice.php
+++ wp-content/themes/healthier-workforce/_parts/free-quotation-advice.php
@@ -4,8 +4,12 @@
 
 				<p>Interested? Get in touch with us today to find out how we can help you.</p>
 
-				<p class="phone"><i class="fa fa-mobile" aria-hidden="true"></i> <strong><?php do_action('ald_default'); ?></strong></p>
+				<?php if ( (isset($_COOKIE['area']) && $_COOKIE['area'] !='uk') || (isset($_GET['a']) && $_GET['a'] !='uk')) : ?>
+			        <p class="phone"><i class="fa fa-mobile" aria-hidden="true"></i> <strong><?php echo do_action('ctm_location'); ?> <?php do_action('ald_default'); ?></strong></p>
+			    <?php else: ?>
+			        <p class="phone"><i class="fa fa-mobile" aria-hidden="true"></i> <strong><?php do_action('ald_default'); ?></strong></p>
+			    <?php endif; ?>
 
-				<a class="btn btn-logodeepblue" href="<?php echo ($siteUrl . '/contact/'); ?>">Contact Us</a>
+				<a class="btn btn-logodeepblue" href="<?php echo site_url(); ?>/contact/">Contact Us</a>
 
 			</div>
