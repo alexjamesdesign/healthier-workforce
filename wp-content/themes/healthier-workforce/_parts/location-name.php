7e374f4a5670c9d2f8885c4585536d480b086ffe
Alex James <hello@alexjamesdesign.co.uk>
2026-04-19 23:01:25 +0100

    Tweaked location-name file to help fix an error in error.log

--- wp-content/themes/healthier-workforce/_parts/location-name.php
+++ wp-content/themes/healthier-workforce/_parts/location-name.php
@@ -1,8 +1,13 @@
 <?php
-$post_id        = get_the_ID();
-$parent_id      = $post_id ? wp_get_post_parent_id( $post_id ) : 0;
-$parent_slug    = $parent_id ? get_post_field( 'post_name', $parent_id ) : '';
-$current_title  = $post_id ? get_the_title( $post_id ) : '';
+$post_id       = get_the_ID();
+
+if ( ! $post_id ) {
+	return;
+}
+
+$parent_id     = wp_get_post_parent_id( $post_id );
+$parent_slug   = $parent_id ? get_post_field( 'post_name', $parent_id ) : '';
+$current_title = get_the_title( $post_id );
 
 if ( $parent_slug === 'areas-we-cover' ) {
 	echo esc_html( ucwords( strtolower( $current_title ) ) );
